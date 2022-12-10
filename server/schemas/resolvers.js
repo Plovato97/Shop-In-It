638f9af1b2f65cb01945cd85b1f925946719ec64
paul lovato <lovato.paul97@yahoo.com>
2022-12-09 23:24:30 -0500

    dependencies set in server.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,12 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+    Query: {
+    }, 
+
+    Mutation: {
+    }
+}
+
+module.exports = resolvers;
