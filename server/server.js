c46ec4ee1a7019c2b456e99a0d5c276009bee1fe
paul lovato <lovato.paul97@yahoo.com>
2022-12-09 23:41:59 -0500

    server fixes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const db = require("./config/connection");
+const path = require("path");
 
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
