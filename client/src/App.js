016457a4ec4f65bfe6496f95834ddf3d5eb908b6
Benny <benny.le890@gmail.com>
2022-12-14 06:16:25 -0500

    added react-router-dom and jwt-decode

--- client/src/App.js
+++ client/src/App.js
@@ -34,22 +34,20 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Nav />
+            <Routes>
+              <Route path='/' element={<Home />} /> 
+              <Route path='/login' element={<Login />} /> 
+              <Route path='/signup' element={<Signup />} /> 
+            </Routes>
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
