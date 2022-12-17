7a1c707899bebc99db8bb6cc4d5a67cd59959130
Benny <benny.le890@gmail.com>
2022-12-17 03:30:21 -0500

    added navbar function of hiding when scrolling

--- client/src/components/Mobile/NavMobile.jsx
+++ client/src/components/Mobile/NavMobile.jsx
@@ -1,18 +1,32 @@
+import React from 'react';
 import NavShopCar from "../Elements/NavShopCar"
 import { Link } from 'react-router-dom';
 
-const NavMobile = () => {
+function Nav() {
 
-  return (
-    <div>
-        <nav id='nav-mobile'>
+     // Navbar scroll - Javascript
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function() {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            document.getElementById("nav-mobile").style.top = "0";
+        } else {
+            document.getElementById("nav-mobile").style.top = "-150px";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+
+    return (
+        <header className='flex-row px-1'>
+            <nav id='nav-mobile'>
         <Link to='/'>
             <p className='logo'>Shop 'n It</p>
         </Link>
             <NavShopCar></NavShopCar>
         </nav>
-    </div>
-  )
+
+        </header>
+    );
 }
 
-export default NavMobile
+export default Nav;
