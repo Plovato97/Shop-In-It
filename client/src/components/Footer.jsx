3c9cb157e051931b2fad972c637586e298dcbaf4
Benny <benny.le890@gmail.com>
2022-12-21 18:31:00 -0500

    added footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Footer = () => {
   return (
     <section id='footer'>
-        
+        <div>
+          <div>
+            <p>
+            Designed & Built by Paul Lovato, Hector Guerrero, Benny Le, Raenique Walters, & Johan Herrera
+            </p>
+          </div>
+        </div>
     </section>
   )
 }
